c7e0da0330a19c8231dbf1b9677cb7d6a3d5e0fe
YoHaiYo <12si47bun@naver.com>
2023-11-30 22:23:47 +0900

    초기세팅 진행중

---
+++ html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>YoHaYo-그누보드</title>
+  <style>
+    body {
+      text-align: center;      
+    }
+    li {
+      list-style: none;
+    }
+  </style>
+</head>
+<body>
+  <h1><a href="/main">메인사이트 바로가기</a></h1>
+  <h2>YoHaYo Git Repository</h2>
+  <?php include_once("./navi.php") ?>
+</body>
+</html>
---
+++ html/navi.php
@@ -0,0 +1,5 @@
+<ul>
+  <li><a href="https://github.com/YoHaiYo">YoHaiYo-github</a></li>
+  <li><a href="https://github.com/YoHaiYo/Team-SoolDamWha">SoolDamWha</a></li>
+  <li><a href="https://github.com/YoHaiYo/UI-component-for-beginner.github.io">UI-component-for-beginner</a></li>
+</ul>
