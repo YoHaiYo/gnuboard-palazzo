da3a2e83f2a0321e226b7c4917ed19ae51f8c1f8
YoHaiYo <12si47bun@naver.com>
2023-12-03 19:55:10 +0900

    베스킨 헤더로 작업중

--- html/navi.php
+++
@@ -1,5 +0,0 @@
-<ul>
-  <li><a href="https://github.com/YoHaiYo">YoHaiYo-github</a></li>
-  <li><a href="https://github.com/YoHaiYo/Team-SoolDamWha">SoolDamWha</a></li>
-  <li><a href="https://github.com/YoHaiYo/UI-component-for-beginner.github.io">UI-component-for-beginner</a></li>
-</ul>
